fd9505b67e20ace7e66be2c3fb48acfbe8482d08
Isaactangky <75126969+Isaactangky@users.noreply.github.com>
2023-01-12 22:00:37 +0800

    config deploy

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -2,8 +2,10 @@ import rootReducer from "./rootReducer";
 import { logger } from "redux-logger";
 import { compose, createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-const middlewares = [process.env.NODE_ENV === "development" && logger, thunk];
-
+const middlewares = [
+  process.env.NODE_ENV === "development" && logger,
+  thunk,
+].filter(Boolean);
 const composeEnhancer =
   (process.env.NODE_ENV !== "production" &&
     window &&
