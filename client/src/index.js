17ea97178db05c5f7f3aa2cbc7cb16ab11e61cf3
Isaactangky <75126969+Isaactangky@users.noreply.github.com>
2023-01-12 21:22:46 +0800

    config deploy

--- client/src/index.js
+++ client/src/index.js
@@ -13,7 +13,7 @@ root.render(
     <Provider store={store}>
       {/* <BrowserRouter> */}
       {/* <BrowserRouter location={history.location} navigator={history}> */}
-      <BrowserRouter history={history}>
+      <BrowserRouter history={history} basename={process.env.PUBLIC_URL}>
         <App />
         {/* </BrowserRouter> */}
       </BrowserRouter>
