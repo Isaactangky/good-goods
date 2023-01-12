0388213e58df09dbc8bd02921f6388e490b560c5
Isaactangky <75126969+Isaactangky@users.noreply.github.com>
2023-01-12 18:27:39 +0800

    deployment

--- client/src/routes/home/home.component.jsx
+++ client/src/routes/home/home.component.jsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
 import Button from "../../components/Button/Button.component";
-
 import { Wrapper, ButtonContainer } from "./home.styles.js";
 import BackgroundImage from "../../images/background.jpg";
 const Home = () => {
